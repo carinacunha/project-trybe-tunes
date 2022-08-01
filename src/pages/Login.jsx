df8d253b7a44ee06bc171ccb0f859823d6dc5cea
Carina Cunha <carina.cunha13@gmail.com>
2022-08-01 15:38:21 -0300

    Requisito 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,23 +5,9 @@ class Login extends Component {
     return (
       <div data-testid="page-login">
         Login
-        <form>
-          <label htmlFor="name">
-            Nome
-            <input
-              data-testid="login-name-input"
-              name="name-input"
-              type="text"
-              id="name"
-              value={ cardName }
-              onChange={ onInputChange }
-            />
-          </label>
-          <button data-testid="login-submit-button" type="button">Entrar</button>
-        </form>
+
       </div>
     );
   }
 }
-
 export default Login;
