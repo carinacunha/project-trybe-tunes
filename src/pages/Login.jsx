ee331aa62c94898c2d84207a844ab3b23d63009f
Carina Cunha <carina.cunha13@gmail.com>
2022-08-01 15:12:33 -0300

    Requisito 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,23 @@ import React, { Component } from 'react';
 class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        Login
+        <form>
+          <label htmlFor="name">
+            Nome
+            <input
+              data-testid="login-name-input"
+              name="name-input"
+              type="text"
+              id="name"
+              value={ cardName }
+              onChange={ onInputChange }
+            />
+          </label>
+          <button data-testid="login-submit-button" type="button">Entrar</button>
+        </form>
+      </div>
     );
   }
 }
